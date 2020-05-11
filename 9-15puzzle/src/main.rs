d9d30ff2814dab7ad4cacc7cca544e760fe55bff
Breno Viana <brenomfviana@gmail.com>
2020-05-11 14:41:27 -0300

    updates comments

--- 9-15puzzle/src/main.rs
+++ 9-15puzzle/src/main.rs
@@ -12,7 +12,7 @@ type Cost = Option<usize>; // None represents positive infinity
 const D: usize = 4;
 /// Posible move directions.
 const DIRS: &'static [(i32, i32)] = &[(-1, 0), (1, 0), (0, -1), (0, 1)];
-/// Board goal
+/// Board goal.
 const GOAL: &'static [&'static [usize]] =
   &[&[1, 2, 3, 4], &[5, 6, 7, 8], &[9, 10, 11, 12], &[13, 14, 15, 0]
 ];
