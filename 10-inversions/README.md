87c2705f4dacbe4772de5acd78dadc1881f6e7d5
Breno Viana <brenomfviana@gmail.com>
2020-05-11 15:03:39 -0300

    adds a solution for array inversions calculation

---
+++ 10-inversions/README.md
@@ -0,0 +1,36 @@
+# Inversion
+
+## Description
+
+Write a program that count the number of inversions of an array.
+
+
+## Input
+
+The first line of input contains an integer T denoting the no of test cases.
+Then T test cases follow. Each test case contains two lines.
+The first line contains an integer n (1 <= n <= 10000)
+denoting the size of the array.
+The next line contains the array values (1<= v <= 30000).
+
+### Sample Input
+
+```
+2
+10
+1 2 3 4 5 6 7 8 9 10
+4
+8 4 2 1
+```
+
+
+## Output
+
+For each case, output the number of inversions of each case.
+
+### Sample Output
+
+```
+0
+6
+```
---
+++ 10-inversions/src/main.rs
@@ -0,0 +1,76 @@
+use std::io;
+
+/// Concats two arrays, counts the number of inversions and returns the number
+/// of inversions and the concated array.
+fn concat(arr1: Vec<usize>, arr2: Vec<usize>) -> (usize, Vec<usize>) {
+  // Get lengths
+  let (larr1, larr2) = (arr1.len(), arr2.len());
+  // Control variables
+  let (maxlen, mut i, mut j) = (larr1 + larr2, 0, 0);
+  // Inversions counter and resulting array
+  let (mut inv, mut arr): (usize, Vec<usize>) = (0, vec![]);
+  // Concat arrays
+  while maxlen > arr.len() {
+    if larr1 > i && larr2 > j && arr1[i] <= arr2[j] {
+      arr.push(arr1[i]);
+      i += 1;
+    } else if larr1 > i && larr2 > j && arr1[i] > arr2[j] {
+      arr.push(arr2[j]);
+      j += 1;
+      inv += 1;
+    } else if larr1 > i && larr2 <= j {
+      arr.push(arr1[i]);
+      i += 1;
+      if arr.len() > 0 && larr2 > 1 { inv += 1; }
+    } else if larr1 <= i && larr2 > j {
+      arr.push(arr2[j]);
+      j += 1;
+    }
+  }
+  (inv, arr)
+}
+
+/// Finds the number of inversions of an array.
+fn solve(array: Vec<usize>) -> (usize, Vec<usize>) {
+  // Get array size
+  let size = array.len();
+  // Check if the array has only one element
+  if size == 1 { return (0, array); }
+  // Calculate the mid
+  let mid = size / 2;
+  // Keep searching
+  let (arr1, arr2) = array.split_at(mid);
+  let (i1, arr1) = solve(arr1.to_vec());
+  let (i2, arr2) = solve(arr2.to_vec());
+  // Concat arrays
+  let (inv, arr) = concat(arr1, arr2);
+  (inv + i1 + i2, arr)
+}
+
+fn main() {
+  // Read number of test cases
+  let mut input = String::new();
+  io::stdin().read_line(&mut input)
+    .expect("Error: Unable to read user input.");
+  let mut itc = input.trim().to_string().parse::<usize>().unwrap();
+  // Run test cases
+  while itc > 0 {
+    // Read array size
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)
+      .expect("Error: Unable to read user input.");
+    let _ = input.trim().to_string().parse::<usize>().unwrap();
+    // Read array
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)
+      .expect("Error: Unable to read user input.");
+    let array: Vec<usize> = input.split(" ")
+      .map(|s| s.trim().to_string().parse::<usize>())
+      .filter_map(Result::ok).collect();
+    // Find the number of inversions
+    let (inv, _) = solve(array);
+    println!("{}", inv);
+    // Next test case
+    itc -= 1;
+  }
+}
