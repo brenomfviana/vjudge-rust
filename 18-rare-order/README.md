611a3ef9ff9e5aaaecc0571aea2d4b459e122087
Breno Viana <brenomfviana@gmail.com>
2020-05-24 17:08:16 -0300

    adds solution for Rare Order problem

---
+++ 18-rare-order/README.md
@@ -0,0 +1,47 @@
+# Rare Order
+
+## Description
+
+A rare book collector recently discovered a book written in an unfamiliar
+language that used the same characters as the English language. The book
+contained a short index, but the ordering of the items in the index was
+different from what one would expect if the characters were ordered the same
+way as in the English alphabet. The collector tried to use the index to
+determine the ordering of characters (i.e., the collating sequence) of the
+strange alphabet, then gave up with frustration at the tedium of the task.
+
+You are to write a program to complete the collector's work. In particular,
+your program will take a set of strings that has been sorted according to a
+particular collating sequence and determine what that sequence is.
+
+
+## Input
+
+The input consists of an ordered list of strings of uppercase letters, one
+string per line. Each string contains at most 20 characters. The end of the
+list is signalled by a line with the single character '#'. Not all letters are
+necessarily used, but the list will imply a complete ordering among those
+letters that are used.
+
+### Sample Input
+
+```
+XWY
+ZX
+ZXY
+ZXW
+YWWX
+#
+```
+
+
+## Output
+
+Your output should be a single line containing uppercase letters in the order
+that specifies the collating sequence used to produce the input data file.
+
+### Sample Output
+
+```
+XZYW
+```
---
+++ 18-rare-order/src/main.rs
@@ -0,0 +1,95 @@
+use std::io;
+use std::cmp;
+use std::collections::{HashMap, VecDeque};
+
+// Lisf of the adjacency vectors
+type AdjLst = Vec<char>;
+// Adjacency Matrix
+type AdjMtx = HashMap<char, AdjLst>;
+// In-degree of vertices
+type InDegree = HashMap<char, usize>;
+
+/// Apply topological sorting (based on Kahn’s algorithm) on the vocabulary
+/// (graph) and return a string with the sorted nodes.
+fn solve(grph: AdjMtx) -> String {
+  // Create a vector to store in-degrees of all vertices
+  let mut indegree: InDegree = HashMap::new();
+  for node in grph.keys() { indegree.insert(*node, 0); }
+  // Traverse adjacency lists to fill in-degrees of vertices | complexity=O(V+E)
+  for node in grph.keys() {
+    for nghbr in &grph[&node] { indegree.insert(*nghbr, indegree[&nghbr] + 1); }
+  }
+  // Initialize deque and enqueue all vertices with in-degree 0
+  let mut deque: VecDeque<char> = VecDeque::new();
+  for (node, indgr) in &indegree { if *indgr == 0 { deque.push_back(*node); } }
+  // Initialize count of visited vertices
+  let mut cnt: usize = 0;
+  // Order of the words in the strange alphabet
+  let mut order = String::new();
+  // Run DFS
+  while let Some(node) = deque.pop_front() {
+    // Add the node to topological order
+    order.push(node);
+    // Iterate through all its neighbouring nodes of dequeued node u and
+    for nghbr in &grph[&node] {
+      // Decrease the neighbor in-degree by 1
+      indegree.insert(*nghbr, indegree[&nghbr] - 1);
+      // If in-degree becomes zero, add it to queue
+      if indegree[nghbr] == 0 { deque.push_back(*nghbr); }
+    }
+    cnt += 1;
+  }
+  // Check if there was a cycle
+  if cnt != grph.keys().len() {
+    println!("There exists a cycle in the graph\n");
+    return String::from("");
+  }
+  // Return final result
+  order
+}
+
+fn main() {
+  loop {
+    // Auxiliary variables
+    let (mut pln, mut ln): (Vec<char>, Vec<char>); ln = vec![];
+    // Create adjacency matrix
+    let mut grph: AdjMtx = HashMap::new();
+    // Read the input
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)
+      .expect("Error: Unable to read user input.");
+    // Check if the end condition was reached
+    if input == "" { break; }
+    // Read the first line
+    pln = input.split("").map(|s| s.trim().parse::<char>())
+      .filter_map(Result::ok).collect();
+    // Read all words
+    while ln != vec!['#'] {
+      // Check if the next line was read
+      if ln != vec![] {
+        // Compare current line with the previous one
+        for i in 0..(cmp::min(ln.len(), pln.len())) {
+          // If the `i`-th char of both lines are different then relate them
+          if ln[i] != pln[i] {
+            // Connect nodes
+            if let Some(adjlst) = grph.get_mut(&pln[i]) { adjlst.push(ln[i]); }
+            else { grph.insert(pln[i], vec![ln[i]]); }
+            // If the node was not added yet then add it
+            if let None = grph.get_mut(&ln[i]) { grph.insert(ln[i], vec![]); }
+            break;
+          }
+        }
+        // Update previous line
+        pln = ln;
+      }
+      // Read the next line
+      let mut input = String::new();
+      io::stdin().read_line(&mut input)
+        .expect("Error: Unable to read user input.");
+      ln = input.split("").map(|s| s.trim().parse::<char>())
+        .filter_map(Result::ok).collect();
+    }
+    // Run topological sort and print result
+    println!("{}", solve(grph));
+  }
+}
